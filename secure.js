b290cce1a4f1b885c20aaa28122c4e176aa2cc50
James Roundy <126376690+swoops503@users.noreply.github.com>
2023-03-21 15:51:04 -0700

    Add files via upload
    uploading components of block07 project: Secure The Vault

---
+++ secure.js
@@ -0,0 +1,16 @@
+// add a string 'You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: 
+
+// assign 3 variables using a unique operator for each, with the equation equalling one of the 3 codes in the combination
+
+// the combination is 10 - 40 - 39
+
+let str= "You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: "; // string variable
+
+let combination1= 50/5; // unique operator 1 / 
+let combination2= 2*20; // unique operator 2 *
+let combination3= 42-3; // unique operator 3 -
+
+console.log(str+combination1+" "+combination2+" "+combination3); // test to verify lines 7-11 work correctly
+
+alert(str+combination1+" "+combination2+" "+combination3); // popup dialog
+
